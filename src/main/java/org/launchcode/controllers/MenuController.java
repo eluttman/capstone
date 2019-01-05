9999905e0acfa2b8c138d2193533bccd6f5c561c
Emily Luttman <emckeever76@gmail.com>
2019-01-05 11:26:27 -0600

    first commit

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,66 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CategoryDao;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    // Request path: /manu
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+
+        return "menu/index";
+    }
+    @RequestMapping(value ="", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+    }
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId()
+    }
+
+    @RequestMapping(value ="view/{id}", method = RequestMethod.GET)
+    public String viewMenu(Model model, @PathVariable int id) {
+        Menu menu = menuDao.findOne(id);
+        model.addAttribute("title", menu.getName());
+        model.addAttribute("menu", menu);
+        return "menu/add";
+    }
+}
